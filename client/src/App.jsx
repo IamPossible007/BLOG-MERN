4d6ba92580e047eadaf3b7509369246422cf70a6
IamPossible007 <113297734+IamPossible007@users.noreply.github.com>
2024-09-17 22:15:33 +0530

    created pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,24 @@
-
+import { Route } from "react-router-dom";
+import { Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Dashboard from "./pages/Dashboard";
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 export default function App() {
   return (
-    <div className="text-3xl text-red-200">
-      hii
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+
+      </Routes>
+    </BrowserRouter>
   )
 }
