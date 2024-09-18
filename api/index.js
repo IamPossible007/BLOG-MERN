161c70f3512ce00bd98249615b84c66133fd529f
IamPossible007 <113297734+IamPossible007@users.noreply.github.com>
2024-09-18 19:12:47 +0530

    sign up page is functional

--- api/index.js
+++ api/index.js
@@ -1,13 +1,33 @@
 import express from 'express';
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
-dotenv.config()
-const app = express();  
 import userRoutes from './routes/user.route.js'; 
+import authRoutes from './routes/auth.route.js'; 
+
+
+dotenv.config()
+
+const app = express();  
+app.use(express.json());
 mongoose.connect(process.env.MONGODB_URI).then(()=>{
     console.log("Connected to MongoDB");
 }).catch((err)=>{
     console.log(err);
 });
+
+
 app.listen(3000, () => console.log('Server running on port 3000'));
-app.use('/api/user', userRoutes); // Fix this line
+
+app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
+
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+      success: false,
+      statusCode,
+      message,
+    });
+  });
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,5 +1,3 @@
-
-
 export default function SignIn() {
   return (
     <div>SignIn</div>
