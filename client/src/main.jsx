ebacc400aa269752393174521dfe1876443b9648
IamPossible007 <113297734+IamPossible007@users.noreply.github.com>
2024-09-19 14:22:22 +0530

    Redux implemented

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+import { persistor, store } from './redux/Store.js';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <PersistGate persistor={persistor}>
+
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  </PersistGate>
 )
